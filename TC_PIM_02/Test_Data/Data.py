ead8a595ae416036fcdc1feae87dbf184c35263f
Mohan-46 <mohanakrishnar2001@gmail.com>
2023-08-16 14:01:21 +0530

    Removed previous commits

---
+++ TC_PIM_02/Test_Data/Data.py
@@ -0,0 +1,13 @@
+class Hrm_data:
+    url ="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username           = "Admin"
+    password           = "admin123"
+    NickName           = 'Mohan'
+    employeid          = '8'
+    otherId            = '6960723423'
+    DriverLicense      = 'TN1900891739'
+    SSN_Number         = '1283494782'
+    SinNumber          = '1286339022'
+    Military_Service   = 'No'
+    DateofBirth        = '2001-11-11'
+    LicenseExpiryDate  = '2041-11-10'
